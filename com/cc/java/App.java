2e1ef3a55c93677caa572c043370a8fc88416db0
Peter Schmidt <schmidt.multimedia@gmail.com>
2024-05-31 08:43:29 +0200

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,39 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+     
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+        Bird bird = new Bird();
+
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+        pollObj(bird);
+
+        //-- Das geht jetzt nicht mehr ! -- // 
+        // HoneyBee bee = new HoneyBee(); // abstrakte Superklasse!
+        // pollObj(bee);
+
+        
+    }
+
+    // ---- 4. Iteration Polymorphie -----//
+    private static void pollObj(HoneyBee obj){
+        output(obj.doYourJob());
+        output(obj.fly());
+        output("-----------------");
+    }
+
+   
+    private static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
+
+
+}
+
